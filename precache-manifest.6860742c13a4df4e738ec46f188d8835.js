edb28dbdc5b0d18a847891c70c8b140e3b8b40c7
Hannah <hannahbeardmore1@hotmail.co.uk>
2020-05-13 20:02:30 +0100

    Updates

---
+++ precache-manifest.6860742c13a4df4e738ec46f188d8835.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a82dcb0b96c848d06106a36bd107248",
+    "url": "/ToDoAppReact/index.html"
+  },
+  {
+    "revision": "ecfd5ee5214e8d4fc655",
+    "url": "/ToDoAppReact/static/css/main.a8282e6e.chunk.css"
+  },
+  {
+    "revision": "82fc9ee3ba297af3cb97",
+    "url": "/ToDoAppReact/static/js/2.25570f8a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ToDoAppReact/static/js/2.25570f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfd5ee5214e8d4fc655",
+    "url": "/ToDoAppReact/static/js/main.8c6731c1.chunk.js"
+  },
+  {
+    "revision": "403eae44a9c5a2660049",
+    "url": "/ToDoAppReact/static/js/runtime-main.b3ab0dd1.js"
+  },
+  {
+    "revision": "e4167d648fc1f5cf52da44446e062271",
+    "url": "/ToDoAppReact/static/media/homeTitle.e4167d64.jpg"
+  }
+]);
